fbf19abf987768acef988777cdc6c8efb54c81be
Totolp24 <thomaslepeu24@gmail.com>
2024-12-29 21:57:29 +0100

    feature: logo & style

--- client/component/Header.tsx
+++ client/component/Header.tsx
@@ -46,33 +46,40 @@ export default function Header() {
   };
 
   return (
-    <header className="w-full bg-header text-black dark:text-white  shadow-md py-8">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between">
-        <div className="mr-auto" >
-        <a href="/" className="block overflow-hidden">
-  <img src="/logo/logo.webp" className="w-5 object-cover transform hover:scale-110 transition-transform duration-300"/>
-</a>
+    <header className="w-full bg-header text-black dark:text-white shadow-md py-8">
+      <div className="max-w-12xl mx-auto px-8 sm:px-8 lg:px-12 flex items-center justify-between">
+        <div className="mr-auto">
+          <a href="/">
+            <img
+              src="/logo/logo.webp"
+              className="w-5 object-cover transform hover:scale-110 transition-transform duration-300"
+            />
+          </a>
         </div>
-        <nav className="flex items-center gap-8">
-          <Link href="/allPost" className=" hover:text-blue-200 text-lg px-6 py-3 rounded-md">
+        <nav className="flex items-center "> {/* Espacement entre les liens de navigation */}
+          <Link href="/allPost" className="hover:text-blue-200 text-lg px-6 py-3 rounded-md ">
             Feed
           </Link>
-          <Link href="/addPost" className=" hover:text-blue-200 text-lg px-6 py-3 rounded-md">
-            add a post
+          <div>/       /   </div>
+          <Link href="/addPost" className="hover:text-blue-200 text-lg px-6 py-3 rounded-md">
+            Add a Post
           </Link>
         </nav>
-        <form onSubmit={(e) => {
-          e.preventDefault();
-          if (isClient) {
-            window.location.href = `/search?query=${searchQuery}`;
-          }
-        }} className="flex items-center gap-4">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (isClient) {
+              window.location.href = `/search?query=${searchQuery}`;
+            }
+          }}
+          className="flex items-center gap-8"
+        >
           <input
             type="text"
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
             placeholder="Search..."
-            className="px-6 py-3 text-black dark:text-white  dark:bg-gray-800 dark:border-gray-800  rounded-md" 
+            className="px-6 py-3 text-black dark:text-white dark:bg-gray-800 dark:border-gray-800 rounded-md"
           />
           <button type="submit" className="border-2 border-black dark:border-white px-6 py-3 rounded-md hover:bg-blue-700">
             Search
@@ -81,15 +88,17 @@ export default function Header() {
         <div className="flex items-center gap-4">
           {!user ? (
             <>
-              <Link href="/login" className=" px-6 py-3 rounded-md hover:bg-blue-700">
-                login
+              <Link href="/login" className="px-6 py-3 rounded-md hover:bg-blue-700">
+                Login
               </Link>
+     
+                <div>/</div>
               <Link href="/signup" className="px-6 py-3 rounded-md hover:bg-blue-700">
-                sign up
+                Sign Up
               </Link>
             </>
           ) : (
-            <Link href="/Profil" className=" px-6 py-3 rounded-md hover:bg-blue-700">
+            <Link href="/Profil" className="px-6 py-3 rounded-md hover:bg-blue-700">
               Profil
             </Link>
           )}
@@ -100,20 +109,16 @@ export default function Header() {
               className="border-2 px-6 py-3 border-red-500"
               onClick={() => supabase.auth.signOut().then(() => setUser(null))}
             >
-              disconnect
+              Disconnect
             </button>
           )}
         </div>
         <div className="ml-4">
-          <button
-            className=" px-6 py-3 rounded-md hover:bg-blue-700"
-            onClick={toggleDarkMode}
-          >
-             <img
-          src={isDarkMode ? "/logo/light-mode.png" : "/logo/dark-mode.png"}
-
-          className="object-cover h-5"
-        />
+          <button className="px-6 py-3 rounded-md hover:bg-blue-700" onClick={toggleDarkMode}>
+            <img
+              src={isDarkMode ? "/logo/light-mode.png" : "/logo/dark-mode.png"}
+              className="object-cover h-5"
+            />
           </button>
         </div>
       </div>
